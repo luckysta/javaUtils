83fac39e508b77c7f2329fd4ffa61ee147a1e449
haojie.sun <haojie.sun@yeepay.com>
2019-02-22 10:33:51 +0800

    项目创建

---
+++ src/main/java/com/minisun/javaUtils/JavaUtilsApplication.java
@@ -0,0 +1,15 @@
+package com.minisun.javaUtils;
+
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,MybatisAutoConfiguration.class})
+public class JavaUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUtilsApplication.class, args);
+	}
+
+}
