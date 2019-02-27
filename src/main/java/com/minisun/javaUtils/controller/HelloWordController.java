cdf65b44d6ea35c86a70bb5db49c7dcd160b5d0b
haojie.sun <haojie.sun@yeepay.com>
2019-02-27 09:50:21 +0800

    项目结构设计

--- src/main/java/com/minisun/javaUtils/controller/HelloWordController.java
+++ src/main/java/com/minisun/javaUtils/controller/HelloWordController.java
@@ -1,12 +1,21 @@
 package com.minisun.javaUtils.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller
 public class HelloWordController {
+    Logger logger = LoggerFactory.getLogger(HelloWordController.class);
     @RequestMapping("/hello")
     public String index(){
         return "welcome to minisun's Java project";
+    }
+    @RequestMapping(value = "/minisun",method = RequestMethod.GET)
+    public String minisun(){
+        logger.info("go to minisun");
+        return "minisun";
     }
 }
